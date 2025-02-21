50da5a6da803e37a1ea38ca63875cc0eda0682ef
[name] <rohitamanrazz@gmail.com>
2025-02-21 16:26:15 +0530

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,80 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/ykte-feature-graphic.png",
+    title: "YouKnowTheEnding",
+    description:
+      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
+    tech_stack: ["React Native", "Google Cloud", "NextJs", "TailwindCSS"],
+    github_url: "",
+    demo_url: "https://youknowtheending.net/",
+  },
+  {
+    img: "/renr-responsive.png",
+    title: "RENR Safety App",
+    description:
+      "A safety application for the Renewable Resources Dept. of BCIT",
+    tech_stack: ["React", "Firebase"],
+    github_url: "",
+    demo_url: "https://renr-bcit.web.app/",
+  },
+  {
+    img: "/mymind-responsive.png",
+    title: "MyMind",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "https://github.com/salesp07/MyMind---Copy",
+    demo_url: "https://mymindweb.vercel.app/",
+  },
+  {
+    img: "/pokedex-responsive.png",
+    title: "Pokedex",
+    description:
+      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
+    tech_stack: ["ReactJs", "NodeJs", "MongoDB"],
+    github_url: "https://github.com/salesp07/Pokedex-ReactApp",
+    demo_url: "https://pokedex-salesp07.netlify.app/",
+  },
+  {
+    img: "/jobDetect.png",
+    title: "JobDetect",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
+    github_url: "https://github.com/salesp07/JobDetectNode",
+    demo_url: "https://jobdetective.onrender.com/",
+  },
+];
+
+export const videos = [
+  {
+    id: 1, 
+    title: "video 1",
+    url: "/video 03.mp4"
+  }
+]
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "/vietnam.png",
+    name: "VN Software",
+  },
+  {
+    img: "premiere-pro.png",
+    name: "Adobe Premiere Pro",
+  },
+  {
+    img: "/final-cut.png",
+    name: "Final Cut Pro",
+  },
+  {
+    img: "/filmora.png",
+    name: "Wondershare Filmora:",
+  },
+  {
+    img: "/canva-icon.png",
+    name: "Canva",
+  },
+];
