bca24573a48d9dd23192bb17c77ef62390b6c6f4
[name] <rohitamanrazz@gmail.com>
2025-02-22 18:56:57 +0530

    fix: add videos

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -36,39 +36,39 @@ export const ProjectList = [
     demo_url: "/video5.mp4",
   },
   {
-    img: "/demo1.png",
-    title: "Video 1",
+    img: "/demo6.png",
+    title: "Video 6",
     description:
-      "This is video 1",
-    demo_url: "https:",
+      "This is video 6",
+    demo_url: "/video6.mp4",
   },
   {
-    img: "/demo2.png",
-    title: "Video 2",
+    img: "/demo7.png",
+    title: "Video 7",
     description:
-      "This is video 2",
-    demo_url: "https:",
+      "This is video 7",
+    demo_url: "/video7.mp4",
   },
   {
-    img: "/demo3.png",
-    title: "Video 3",
+    img: "/demo8.png",
+    title: "Video 8",
     description:
-      "This is video 3",
-    demo_url: "https:",
+      "This is video 8",
+    demo_url: "/video8.mp4",
   },
   {
-    img: "/demo4.png",
-    title: "Video 4",
+    img: "/demo9.png",
+    title: "Video 9",
     description:
-      "This is video 4",
-    demo_url: "https:",
+      "This is video 9",
+    demo_url: "/video9.mp4",
   },
   {
-    img: "/demo5.png",
-    title: "Video 5",
+    img: "/demo10.png",
+    title: "Video 10",
     description:
-      "This is video 5",
-    demo_url: "https:",
+      "This is video 10",
+    demo_url: "/video10.mp4",
   }
 ];
 // --------------------------------------- Skills
